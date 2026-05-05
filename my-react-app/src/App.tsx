c45ad8bcfe9b6be011a4d81485c179335835079e
Gau2756 <gau2756@gmail.com>
2026-05-05 16:16:51 -0400

    Initial

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import ProblemSet from './ProblemSet.tsx';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <h1>Math Homework</h1>
+         <ProblemSet></ProblemSet>
+      </div>
+  )
+}
+
+export default App
+
